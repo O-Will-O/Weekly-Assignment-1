62ed8882d5e2724b6855c6ab56105a0cb6825901
JaidenDenlinger <jaidendenlinger05@gmail.com>
2023-10-12 09:31:35 -0400

    add a few base functions

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ function divideOneByX()
     equation = `1/${num}`;
     
 }
+
+function square()
+{
+    equation = `${num}^2`;
+}
+
+function sqrt()
+{
+    equation = `√${num}`;
+}
+
+function switchSign()
+{
+    equation = `-${num}`;
+}
